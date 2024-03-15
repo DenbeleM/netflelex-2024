15109f5510477a9b57cd8d2c3f288c8bcd7c8eea
DenbeleM <tibebu.kejela-ug@aau.edu.et>
2024-03-15 12:43:22 -0300

    first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Component/Header/Header";
+import Footer from "../Component/Footer/Footer";
+
+const home = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default home;
